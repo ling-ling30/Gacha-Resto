8a9b25dc88aafd0b01e75677a3564dadbc7e5878
Ricky <ricky-inbox@outlook.com>
2024-06-24 11:51:36 +0700

    feat: search feature

--- components/Restaurant.tsx
+++ components/Restaurant.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Accordion,
   AccordionContent,
@@ -7,39 +7,58 @@ import {
 } from "@/components/ui/accordion";
 import Photo from "./Photo";
 import { Id } from "@/convex/_generated/dataModel";
+import { Button } from "./ui/button";
+import { AddPhotoDialog } from "./DialogAddPhoto";
+import { Input } from "./ui/input";
 
 type Props = {
-  restaurants:
-    | {
-        _id: Id<"restaurant">;
-        _creationTime: number;
-        name: string;
-        city: Id<"city">;
-        price_range_min: string;
-        price_range_max: string;
-      }[]
-    | undefined;
+  restaurants: {
+    _id: Id<"restaurant">;
+    _creationTime: number;
+    name: string;
+    city: Id<"city">;
+    price_range_min: string;
+    price_range_max: string;
+  }[];
 };
 
 export default function Restaurant({ restaurants }: Props) {
+  const [search, setSearch] = useState("");
+
+  const filteredRestaurants = restaurants.filter((restaurant) =>
+    restaurant.name.toLowerCase().includes(search.toLowerCase())
+  );
   return (
     <Accordion type="single" collapsible>
       <AccordionItem value="item-1">
         <AccordionTrigger>Semua Restoran</AccordionTrigger>
         <AccordionContent className="py-10 px-15">
-          {restaurants
-            ? restaurants?.map((item, index) => {
+          <Input
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+            className="m-2 w-[300px]"
+            placeholder="Search Restaurant..."
+          />
+          {filteredRestaurants
+            ? filteredRestaurants?.map((item, index) => {
                 return (
-                  <div className="mb-10" key={item._id}>
-                    <div className="font-semibold">
-                      {index + 1}. {item.name}
+                  <div
+                    key={item._id}
+                    className="flex justify-between items-center px-2"
+                  >
+                    <div className="mb-10">
+                      <div className="font-semibold text-lg">
+                        {index + 1}. {item.name}
+                      </div>
+                      <div className="font-semibold">
+                        Rp. {item.price_range_min} - Rp. {item.price_range_max}
+                      </div>
+                      <div className="gap-2 flex flex-wrap">
+                        <Photo restaurant_id={item._id} />
+                      </div>
                     </div>
-                    <div className="font-semibold">
-                      Rp. {item.price_range_min} - Rp. {item.price_range_max}
-                    </div>
-                    <div className="gap-2 flex flex-wrap">
-                      <Photo restaurant_id={item._id} />
-                    </div>
+                    <AddPhotoDialog restaurant_id={item._id} />
                   </div>
                 );
               })
