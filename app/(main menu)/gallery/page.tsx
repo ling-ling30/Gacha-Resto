fd18385aa78249bdb82c0eb796058fddca29132e
Ricky - <rickyjay567@gmail.com>
2024-07-18 23:56:45 +0700

    ui: add suspense

--- app/(main menu)/gallery/page.tsx
+++ app/(main menu)/gallery/page.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import EmblaCarousel from "./_components/EmblaCarousel";
+import { Skeleton } from "@/components/ui/skeleton";
 
 type Props = {};
 
@@ -13,17 +14,19 @@ export default function Page({}: Props) {
   const restaurants = useQuery(api.restaurant.fetchAllRestaurantByCity, {
     city_id: cityId,
   });
-  if (!restaurants) {
-    return "loading..";
-  }
+
   const slides = [1, 2, 3, 4, 5];
   return (
     <div className="w-full flex justify-center items-center h-[80vh]">
       <div className="container mx-auto py-10">
-        <EmblaCarousel
-          slides={restaurants}
-          options={{ loop: true, align: "center" }}
-        />
+        {restaurants ? (
+          <EmblaCarousel
+            slides={restaurants}
+            options={{ loop: true, align: "center" }}
+          />
+        ) : (
+          <Skeleton className="w-full h-[400px] bg-gray-300" />
+        )}
       </div>
     </div>
   );
